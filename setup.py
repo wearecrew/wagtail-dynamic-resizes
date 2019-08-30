ea71319d568591d7e791b74e51d9daa58ed31d45
heyscam <heyscam@gmail.com>
2019-08-30 20:26:06 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="crewcli",
+    version="0.0.1",
+    packages=["wagtail_dynamic_resizes"],
+    install_requires=["wagtail"],
+)
---
+++ wagtail_dynamic_resizes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WagtailPermittedImagesConfig(AppConfig):
+    name = "wagtailpermittedimages"
