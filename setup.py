f6e005eab9c63509eb524f03b5cc1de23c4a47c8
Simon Cam <heyscam@gmail.com>
2021-08-27 10:54:44 +0100

    Remove apps file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wagtail-dynamic-resizes",
-    version="0.0.1",
+    version="0.0.2",
     packages=["wagtail_dynamic_resizes"],
     install_requires=["wagtail"],
 )
--- wagtail_dynamic_resizes/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WagtailPermittedImagesConfig(AppConfig):
-    name = "wagtailpermittedimages"
